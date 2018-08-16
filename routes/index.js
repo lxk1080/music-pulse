91b25cc2186ba188718afdc05a98fdb6d30d3ab2
lxk1080 <266427651@qq.com>
2018-08-17 00:42:57 +0800

    get image from audio

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,35 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const jsmediatags = require('jsmediatags');
 const router = express.Router();
 
 const media = path.join(__dirname, '../public/media');
 
 router.get('/', function(req, res, next) {
   fs.readdir(media, function(err, data) {
-    const lists = data.map(item => item.slice(0, item.length - 4)); // 去掉 '.mp3' 后缀
-    if (!err) {
-      res.render('index', {
-        title: 'music pulse',
-        musicLists: lists,
-      })
-    }
+    // ...data
+    //for (let i = 0; i < data.length; i++) {
+    let image = null;
+      jsmediatags.read(`${media}/${data[data.length - 1]}`, {
+        onSuccess: function(tag) {
+          image = tag.tags.picture;
+
+          const lists = data.map(item => item.slice(0, item.length - 4)); // 去掉 '.mp3' 后缀
+          if (!err) {
+            res.render('index', {
+              title: 'music pulse',
+              musicLists: lists,
+              image: JSON.stringify(image),
+            })
+          }
+
+        },
+        onError: function(error) {
+          console.log(':(', error.type, error.info);
+        }
+      });
+    //}
   })
 });
 
